892abf76b8409204234feb34960b6c889040598e
Ayrat Fatykhov <airat_91_91@mail.ru>
2024-02-27 21:54:01 +0300

    change new page for show product

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,9 +2,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
-from catalog.views import index, contacts
+from catalog.views import index, contacts, show_product
 
 urlpatterns = [
     path('', index),
-    path('contacts/', contacts)
+    path('contacts/', contacts),
+    path('show_product/<int:pk>/', show_product, name='show_product')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
