34e656b5d5809ed074a48bb14140f44b0557759f
Ayrat Fatykhov <airat_91_91@mail.ru>
2024-02-27 22:00:06 +0300

    add template filters

---
+++ catalog/templatetags/my_tags.py
@@ -0,0 +1,16 @@
+from django import template
+
+
+register = template.Library()
+
+
+@register.filter()
+def split(text):
+    return text[:100]
+
+
+@register.filter()
+def mymedia(val):
+    if val:
+        return f'/media/{val}'
+    return '#'
