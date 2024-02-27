bdbf240bafbda7d48d37665bf34cc216279fce8c
Ayrat Fatykhov <airat_91_91@mail.ru>
2024-02-27 21:54:54 +0300

    add new controller for show product

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 def index(request):
-    return render(request, 'catalog/index.html')
+    context = {
+        'object_list': Product.objects.all()
+    }
+    return render(request, 'catalog/index.html', context)
 
 
 def contacts(request):
     return render(request, 'catalog/contacts.html')
+
+
+def show_product(request, pk):
+    context = {'object': Product.objects.get(pk=pk)}
+    return render(request, 'catalog/show_product.html', context)
