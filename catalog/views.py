a79f98bf2b73573da779adac23d558f3181f8ff7
Ayrat Fatykhov <airat_91_91@mail.ru>
2024-03-06 18:55:25 +0300

    change controllers FBV on CBV and add CBV controllers for blog

--- catalog/views.py
+++ catalog/views.py
@@ -1,19 +1,65 @@
-from django.shortcuts import render
-
-from catalog.models import Product
-
-
-def index(request):
-    context = {
-        'object_list': Product.objects.all()
-    }
-    return render(request, 'catalog/index.html', context)
+from django.urls import reverse_lazy, reverse
+from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView, DeleteView
+from pytils.translit import slugify
+from catalog.models import Product, Blog
 
 
-def contacts(request):
-    return render(request, 'catalog/contacts.html')
+class ProductListView(ListView):
+    model = Product
+    template_name = 'catalog/index.html'
 
 
-def show_product(request, pk):
-    context = {'object': Product.objects.get(pk=pk)}
-    return render(request, 'catalog/show_product.html', context)
+class ContactsTemplateView(TemplateView):
+    template_name = 'catalog/contacts.html'
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'catalog/show_product.html'
+
+
+class BlogListView(ListView):
+    model = Blog
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(is_publication=True)
+        return queryset
+
+
+class BlogDetailView(DetailView):
+    model = Blog
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_count += 1
+        self.object.save()
+        return self.object
+
+
+class BlogCreateView(CreateView):
+    model = Blog
+    fields = ('blog_title', 'blog_content', 'blog_preview',)
+    success_url = reverse_lazy('blog_list')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_mat = form.save()
+            new_mat.blog_slug = slugify(new_mat.blog_title)
+            new_mat.save()
+
+        return super().form_valid(form)
+
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    fields = ('blog_title', 'blog_content', 'blog_preview',)
+    # success_url = reverse_lazy('blog_list')
+
+    def get_success_url(self):
+        return reverse('blog_view', args=[self.kwargs.get('pk')])
+
+
+class BlogDeleteView(DeleteView):
+    model = Blog
+    success_url = reverse_lazy('blog_list')
