4582538e53920b894c98bb03e4638d0324d0dc9a
Ayrat Fatykhov <airat_91_91@mail.ru>
2024-03-20 21:26:26 +0300

    add crud for product

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,12 +3,15 @@ from django.conf.urls.static import static
 from django.urls import path
 
 from catalog.views import ProductListView, ProductDetailView, ContactsTemplateView, BlogListView, BlogCreateView, \
-    BlogDetailView, BlogUpdateView, BlogDeleteView
+    BlogDetailView, BlogUpdateView, BlogDeleteView, ProductCreateView, ProductUpdateView, ProductDeleteView
 
 urlpatterns = [
-    path('', ProductListView.as_view()),
-    path('contacts/', ContactsTemplateView.as_view()),
+    path('', ProductListView.as_view(), name='product_list'),
+    path('product/create/', ProductCreateView.as_view(), name='product_create'),
     path('show_product/<int:pk>/', ProductDetailView.as_view(), name='show_product'),
+    path('product/edit/<int:pk>/', ProductUpdateView.as_view(), name='product_edit'),
+    path('product/delete/<int:pk>/', ProductDeleteView.as_view(), name='product_delete'),
+    path('contacts/', ContactsTemplateView.as_view()),
     path('blog/', BlogListView.as_view(), name='blog_list'),
     path('blog/create/', BlogCreateView.as_view(), name='blog_create'),
     path('blog/view/<int:pk>/', BlogDetailView.as_view(), name='blog_view'),
