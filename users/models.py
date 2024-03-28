aee5c51553d1ea303eb18df6d4eb3afd4ac210bf
Ayrat Fatykhov <airat_91_91@mail.ru>
2024-03-28 22:42:34 +0300

    add model user

---
+++ users/models.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class User(AbstractUser):
+    """
+    Определяет поля для модели 'Пользватель'
+    """
+    username = None
+    email = models.EmailField(unique=True, verbose_name='почта')
+
+    avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
+    phone = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE)
+    country = models.CharField(max_length=150, verbose_name='страна', **NULLABLE)
+
+    """
+    Реализуют возможность взаимодействия с пользователем через email
+    (по умолчанию установлено поле 'имя пользователя')
+    """
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
